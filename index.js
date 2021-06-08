730b68ae5db7dc6472970b3c07827854b32be831
Shannon <shannon.nell92@gmail.com>
2021-06-08 09:17:25 -0400

    Array of questions created

--- index.js
+++ index.js
@@ -1,7 +1,75 @@
-// // TODO: Include packages needed for this application
+//node packages
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
+// TODO: Create an array of questions for user input
+const questions = [
+    { 
+        //project information
+        type: 'input', 
+        name: 'title',
+        message: 'What is your project title? (Required)',
+        validate: titleInput => {
+            if(titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title!'); //OR return 'Please enter...'
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if(descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a project description!'); //OR return 'Please enter...'
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do users install your app?',
+    },
+    {
+        type: 'input', 
+        name: 'usage', 
+        message: 'How do users use your app?'   
+    },
+    {
+        type: 'input', 
+        name: 'contribution', 
+        message: 'What are the contribution guidelines for your app?'   
+    },
+    {
+        type: 'input', 
+        name: 'test', 
+        message: 'What are the test instructions for your app?'   
+    },
+    {
+        //license information
+        type: 'list',
+        name: 'license',
+        message: 'What licence did you use?',
+        choices: ['MIT License', 'GNU License', 'Apache License 2.0', 'BSD']
+    },
+    {
+        //user information
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email'
+    }
+];
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
@@ -12,5 +80,14 @@
 // // Function call to initialize app
 // init();
 
-const message = 'hello';
-console.log(message);
+const readmeDataArgs = process.argv.slice(2, process.argv.length);
+const [title, github] = readmeDataArgs;
+
+const generatePage = (projTitle, githubName) => {
+    return `
+    Title: ${projTitle}
+    GitHub: ${githubName}
+    `;
+};
+console.log(title, github);
+console.log(generatePage(title, github));
